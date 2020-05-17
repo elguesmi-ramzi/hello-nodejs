36b44e520305f989a56b703bbef62493b8fcf1b9
elguesmi <elguesmi@hotmail.com>
2020-05-17 11:11:16 +0200

    creation de branche

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // un commantaire pour verifier le commit
 // ajout du 2 eme commentaire
-console.log("hello node js ! \n")
+console.log("hello node js ! \n");
+console.log("on rajoute un deuxieme message");
